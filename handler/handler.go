b0c5731b602060f607d7e5cfad61d85db28b3ca1
Carl Yin <yachenyin820@gmail.com>
2024-04-10 01:37:17 -0700

    Add a new feature to handle POST request in handler & add UploadSucHandler to deal with the successful upload

--- handler/handler.go
+++ handler/handler.go
@@ -1,22 +1,48 @@
 package handler
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 )
 
 // UploadHandler: Deal with file upload
-func UploadHandler(w http.ResponseWriter, r *http.Request){
-	if r.Method == "GET"{
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
 		// return upload html page
 		data, err := os.ReadFile("./static/view/index.html")
-		if err != nil{
+		if err != nil {
 			io.WriteString(w, "internal server error")
 			return
 		}
 		io.WriteString(w, string(data))
-	} else if r.Method == "POST"{
+	} else if r.Method == "POST" {
 		// receive file stream and save to local cataglog
+		file, head, err := r.FormFile("file")
+		if err != nil {
+			fmt.Printf("Failed to get data, err:%s\n", err.Error())
+		}
+		defer file.Close()
+
+		newFile, err := os.Create("/tmp/" + head.Filename)
+		if err != nil {
+			fmt.Printf("Failed to create file, err:%s\n", err.Error())
+			return
+		}
+		defer newFile.Close()
+
+		_, err = io.Copy(newFile, file)
+		if err != nil {
+			fmt.Print("Failed to save data into f")
+			return
+		}
+
+		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
 	}
+}
+
+// UploadSucHandler: generate response when the upload is finished.
+func UploadSucHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Upload finished!")
 }
