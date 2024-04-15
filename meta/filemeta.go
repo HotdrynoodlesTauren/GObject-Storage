cb20634a040756e0841baed89bd4b57cdbe122da
Carl Yin <yachenyin820@gmail.com>
2024-04-15 03:09:56 -0700

    Add the new func to get file meta from database

--- meta/filemeta.go
+++ meta/filemeta.go
@@ -1,5 +1,9 @@
 package meta
 
+import (
+	mydb "gobject-storage/db"
+)
+
 // FileMeta: struct for file meta info
 type FileMeta struct {
 	FileSha1 string
@@ -21,9 +25,30 @@ func UpdateFileMeta(fmeta FileMeta) {
 	fileMetas[fmeta.FileSha1] = fmeta
 }
 
+// UpdateFileMetaDB : 新增/更新文件元信息到mysql中
+func UpdateFileMetaDB(fmeta FileMeta) bool {
+	return mydb.OnFileUploadFinished(
+		fmeta.FileSha1, fmeta.FileName, fmeta.FileSize, fmeta.Location)
+}
+
 // GetFileMeta: get the file meta with file sha1 hash value
 func GetFileMeta(fileSha1 string) FileMeta {
 	return fileMetas[fileSha1]
+}
+
+// GetFileMeta: get the file meta with file sha1 hash value from db
+func GetFileMetaDB(fileSha1 string) (*FileMeta, error) {
+	tfile, err := mydb.GetFileMeta(fileSha1)
+	if tfile == nil || err != nil {
+		return nil, err
+	}
+	fmeta := FileMeta{
+		FileSha1: tfile.FileHash,
+		FileName: tfile.FileName.String,
+		FileSize: tfile.FileSize.Int64,
+		Location: tfile.FileAddr.String,
+	}
+	return &fmeta, nil
 }
 
 // RemoveFileMetaL: remove the file meta with file sha1 hash value
