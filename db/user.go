56929e31a8f8b4c4ba892393675a3ba7c7acf9d7
Carl Yin <yachenyin820@gmail.com>
2024-04-15 03:17:37 -0700

    Add functions to update and get user data from db

---
+++ db/user.go
@@ -0,0 +1,99 @@
+package db
+
+import (
+	"fmt"
+	mydb "gobject-storage/db/mysql"
+)
+
+// User : 用户表model
+type User struct {
+	Username     string
+	Email        string
+	Phone        string
+	SignupAt     string
+	LastActiveAt string
+	Status       int
+}
+
+// UserSignup : save user identity into the mysql db
+func UserSignup(username string, passwd string) bool {
+	stmt, err := mydb.DBConn().Prepare(
+		"insert ignore into tbl_user (`user_name`,`user_pwd`) values (?,?)")
+	if err != nil {
+		fmt.Println("Failed to insert, err:" + err.Error())
+		return false
+	}
+	defer stmt.Close()
+
+	ret, err := stmt.Exec(username, passwd)
+	if err != nil {
+		fmt.Println("Failed to insert, err:" + err.Error())
+		return false
+	}
+	if rowsAffected, err := ret.RowsAffected(); nil == err && rowsAffected > 0 {
+		return true
+	}
+	return false
+}
+
+// UserSignin : decide if the password is correct
+func UserSignin(username string, encpwd string) bool {
+	stmt, err := mydb.DBConn().Prepare("select * from tbl_user where user_name=? limit 1")
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query(username)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	} else if rows == nil {
+		fmt.Println("username not found: " + username)
+		return false
+	}
+
+	pRows := mydb.ParseRows(rows)
+	if len(pRows) > 0 && string(pRows[0]["user_pwd"].([]byte)) == encpwd {
+		return true
+	}
+	return false
+}
+
+// UpdateToken : update user's login token
+func UpdateToken(username string, token string) bool {
+	stmt, err := mydb.DBConn().Prepare(
+		"replace into tbl_user_token (`user_name`,`user_token`) values (?,?)")
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(username, token)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	return true
+}
+
+// GetUserInfo: Query user information
+func GetUserInfo(username string) (User, error) {
+	user := User{}
+	stmt, err := mydb.DBConn().Prepare(
+		"SELECT user_name, signup_at FROM tbl_user WHERE user_name=? LIMIT 1")
+	if err != nil {
+		fmt.Println(err.Error())
+		return user, err
+	}
+	defer stmt.Close()
+
+	// Execute the query
+	err = stmt.QueryRow(username).Scan(&user.Username, &user.SignupAt)
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
